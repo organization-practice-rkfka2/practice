02f3e7b6e5ed932db54de677ddddaf7bba6593ec
thdrkfka <ssongjiaran@gmail.com>
2024-02-16 11:39:50 +0900

    First Commit

---
+++ src/main/java/org/ohgiraffers/gitprojecttest/GitProjectTestApplication.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.gitprojecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProjectTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitProjectTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ohgiraffers/gitprojecttest/GitProjectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.gitprojecttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProjectTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
